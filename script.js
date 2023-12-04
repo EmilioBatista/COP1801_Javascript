abeb7f2e687af4fde2f183e855538b75a60b2296
EmilioBatista <150546349+EmilioBatista@users.noreply.github.com>
2023-12-03 21:28:55 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,42 @@
-// For loop that iterates from 0 to 10
-for (let i = 0; i <= 10; i++) {
-    // Determination and display of odd/even for each number
-    let type = i % 2 === 0 ? 'even' : 'odd';
-    document.write(`Count ${i} is ${type}<br>`);
+// Function to verify valid service quality
+function isValidServiceQuality(serviceQuality) {
+    return serviceQuality === "great" || serviceQuality === "ok" || serviceQuality === "poor";
 }
 
-// Prompt user for a number between 5 and 20, store in myNum
-let myNum = parseInt(prompt("Enter a number between 5 and 20:"));
-while (myNum < 5 || myNum > 20 || isNaN(myNum)) {
-    myNum = parseInt(prompt("Invalid input. Please enter a number between 5 and 20:"));
+// Function to verify service amount is between $5.00 and $500.00
+function isValidServiceAmount(amount) {
+    return amount >= 5 && amount <= 500;
 }
 
-// Do While loop until loop counter equals myNum
-let counter = 1;
-do {
-    document.write(`Counter: ${counter}<br>`);
-    counter++;
-} while (counter <= myNum);
+// Function to calculate the tip amount
+function calculateTip(amount, serviceQuality) {
+    let tipPercentage = 0;
+    switch (serviceQuality) {
+        case "great":
+            tipPercentage = 0.20;
+            break;
+        case "ok":
+            tipPercentage = 0.15;
+            break;
+        case "poor":
+            tipPercentage = 0.10;
+            break;
+    }
+    return amount * tipPercentage;
+}
 
-// Subjects stored in an array
-let subjects = ["Accounting", "Algebra", "Programming", "Art", "Data Analytics"];
+// Prompting the user for the service amount and service quality
+let serviceAmount = parseFloat(prompt("Enter the dollar amount of the service:"));
+let serviceQuality = prompt("Was the service quality great, ok, or poor?").toLowerCase();
 
-// Display array contents using forEach()
-document.write("Subjects:<br>");
-subjects.forEach(subject => {
-    document.write(`${subject}<br>`);
-});
-
-// Display array contents separated by commas in one statement 
-document.write("Subjects (Comma Separated):<br>");
-document.write(subjects.join(", "));
+// Validating service amount and service quality
+if (!isValidServiceAmount(serviceAmount)) {
+    alert("Invalid service amount. Please enter a value between $5.00 and $500.00.");
+} else if (!isValidServiceQuality(serviceQuality)) {
+    alert("Invalid service quality. Please enter great, ok, or poor.");
+} else {
+    // Calculating and displaying the tip
+    let tipAmount = calculateTip(serviceAmount, serviceQuality);
+    alert(`For a service amount of $${serviceAmount.toFixed(2)}, a ${serviceQuality} service quality ` +
+          `suggests a tip of $${tipAmount.toFixed(2)}.`);
+}
